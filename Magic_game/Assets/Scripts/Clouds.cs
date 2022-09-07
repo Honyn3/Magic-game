dfabe1b4ca2d6121f0282240bb0fa85439074b36
Honyn3 <honza.krahula2003@gmail.com>
2022-09-07 20:27:42 +0200

    Menu_Inprovement

--- Magic_game/Assets/Scripts/Clouds.cs
+++ Magic_game/Assets/Scripts/Clouds.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Clouds : MonoBehaviour
 {
-    //DODELAT, SPATNE SE HYBOU MRAKY
 
     public Sprite[] clouds;
 
@@ -15,7 +14,7 @@ public class Clouds : MonoBehaviour
 
     public float spawnX;
 
-    float Speed = 200;
+    private float[] Speed = new float[] {200, 200, 200, 200 };
 
     void Start()
     {
@@ -25,46 +24,48 @@ public class Clouds : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Spawner1.velocity = Vector2.left * Speed * Time.fixedDeltaTime;
+        Spawner1.velocity = Vector2.left * Speed[0] * Time.fixedDeltaTime;
         if (Spawner1.transform.position.x < -spawnX)
         {
             Spawner1.transform.Translate(new Vector2(2 * spawnX, Random.Range(-0.5f, 0.5f)));
-            ChangeCLoud(Spawner1.GetComponent<SpriteRenderer>(), Spawner1.transform);
+            ChangeCLoud(Spawner1.GetComponent<SpriteRenderer>(), Spawner1.transform, 0);
         }
 
-        Spawner2.velocity = Vector2.left * Speed * Time.fixedDeltaTime;
+        Spawner2.velocity = Vector2.left * Speed[1] * Time.fixedDeltaTime;
         if (Spawner2.position.x < -spawnX)
         {
             Spawner2.transform.Translate(new Vector2(2 * spawnX, Random.Range(-0.5f, 0.5f)));
-            ChangeCLoud(Spawner2.GetComponent<SpriteRenderer>(), Spawner2.transform);
+            ChangeCLoud(Spawner2.GetComponent<SpriteRenderer>(), Spawner2.transform,1);
         }
 
-        Spawner3.velocity = Vector2.left * Speed * Time.fixedDeltaTime;
+        Spawner3.velocity = Vector2.left * Speed[2] * Time.fixedDeltaTime;
         if (Spawner3.position.x < -spawnX)
         {
             Spawner3.transform.Translate(new Vector2(2 * spawnX, Random.Range(-2.5f, 2.5f)));
-            ChangeCLoud(Spawner3.GetComponent<SpriteRenderer>(), Spawner3.transform);
+            ChangeCLoud(Spawner3.GetComponent<SpriteRenderer>(), Spawner3.transform,2);
         }
 
-        Spawner4.velocity = Vector2.left * Speed * Time.fixedDeltaTime;
+        Spawner4.velocity = Vector2.left * Speed[3] * Time.fixedDeltaTime;
         if (Spawner4.position.x < -spawnX)
         {
             Spawner4.transform.Translate(new Vector2(2 * spawnX, Random.Range(-0.5f, 0.5f)));
-            ChangeCLoud(Spawner4.GetComponent<SpriteRenderer>(), Spawner4.transform);
+            ChangeCLoud(Spawner4.GetComponent<SpriteRenderer>(), Spawner4.transform,3);
         }
+
 
     }
 
-    private void ChangeCLoud(SpriteRenderer renderer, Transform transform)
+    private void ChangeCLoud(SpriteRenderer renderer, Transform transform, int SpeedIndex)
     {
         renderer.sprite = clouds[(int)Random.Range(0, clouds.Length)];
-        if (transform.position.y > 1) transform.Translate(new Vector2(0, -0.5f));
-        if (transform.position.y < 0.2f)
+        if (transform.position.y > 4.5f) transform.Translate(new Vector2(0, -2.5f));
+        if (transform.position.y < 2f)
         {
-            transform.Translate(new Vector2(0, 0.5f));
-            //Debug.Log(transform.position.y);
+            transform.Translate(new Vector2(0, 2f));
+            
         }
-        
+        //Debug.Log(transform.position.y);
+        Speed[SpeedIndex] = Random.Range(100, 300);
     }
 
 
